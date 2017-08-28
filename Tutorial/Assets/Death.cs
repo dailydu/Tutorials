501d328b983d9a12ba8b8cace5bed2a0eb264fe2
dailydu <daily701@gmail.com>
2017-08-27 21:22:30 -0500

    Death added #3
    Death added #3

---
+++ Tutorial/Assets/Death.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if(collision.gameObject.name == "Player")
+        //{
+            Destroy(collision.gameObject);
+       // }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
